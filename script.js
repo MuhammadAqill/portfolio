40be3e3435ef8b95937e2727b030d5fb8a46f03d
Yutootosaka <maqil5697@gmail.com>
2024-06-21 08:29:06 +0800

    updates

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 // parallax
 $(window).scroll(function () {
   var wScroll = $(this).scrollTop();
+  var speedFactor = 1.2, speedFactor1 = 2;
 
   console.log(wScroll);
 
   $('.jumbotron img').css({
-    transform: 'translate(0px, ' + wScroll / 2.5 + '%)',
+    transform: 'translate(0px, ' + wScroll / 1.4 + '%)',
   });
 
   $('.jumbotron h1').css({
-    transform: 'translate(0px, ' + wScroll / 1.5 + '%)',
+    transform: 'translate(0px, ' + (wScroll * speedFactor) + '%)'
   });
 
   $('.jumbotron p').css({
-    transform: 'translate(0px, ' + wScroll + '%)',
+    transform: 'translate(0px, ' + (wScroll * speedFactor1) + '%)',
   });
 });
